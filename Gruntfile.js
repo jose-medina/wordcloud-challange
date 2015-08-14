4bd507ea70fc85e585bebc5f417f74ffb71afd85
tostaconaceite <tostaconaceite@gmail.com>
2015-08-14 18:05:59 +0200

    project structure, grunt added

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+	require('time-grunt')(grunt);
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		browserify: {
+			dist: {
+				files: {
+					'dist/js/common.js': 'src/js/main.js'
+				}
+			}
+		},
+		handlebars: {
+			compile: {
+				options: {
+					namespace: "JST",
+					node: true
+				},
+				files: {}
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {}
+			}
+		}
+	});
+
+	grunt.registerTask('test', ['']);
+
+	grunt.registerTask('build', [
+		'handlebars',
+		'browserify'
+	]);
+
+	grunt.registerTask('default', ['build']);
+
+};
